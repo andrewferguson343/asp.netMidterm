283df68e96f55ff5225571df7a10ece317b1351c
Andrew Ferguson <you@example.com>
2018-09-11 18:40:26 -0400

    second try

---
+++ web.js
@@ -0,0 +1,21 @@
+﻿
+function initializeBookView() {
+  var elements = document.querySelectorAll(".bookCover");
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].style.display = "";
+    elements[i].style.display = "inline-block";
+  }
+}
+function filterBooks() {
+  var elements = document.querySelectorAll(".bookCover");
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].style.display = "";
+  }
+
+  var genre = "." + document.getElementById("genreFilter").value;
+  var displayBooks = document.querySelectorAll(genre);
+
+  for (i = 0; i < displayBooks.length; i++) {
+    displayBooks[i].style.display = "inline-block";
+  }
+}
