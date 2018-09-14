fa2cb6f463e92a905c1ef062574cfa86d6010dc2
Andrew Ferguson <you@example.com>
2018-09-14 13:05:11 -0400

    validation working

--- web.js
+++ web.js
@@ -5,11 +5,12 @@
     elements[i].style.display = "inline-block";
   }
 }
+
 function filterBooks() {
   var elements = document.querySelectorAll(".bookCover");
   for (var i = 0; i < elements.length; i++) {
     elements[i].style.display = "";
-  }
+}
 
   var genre = "." + document.getElementById("genreFilter").value;
   var displayBooks = document.querySelectorAll(genre);
@@ -18,20 +19,19 @@ function filterBooks() {
     displayBooks[i].style.display = "inline-block";
   }
 }
+
 function validateRegistrationForm() {
   var errorMessage = "";
   var error = document.getElementById("errorMessage");
-  error.appendChild(document.createTextNode(""));
-
-
-  alert("validation hit");
+  error.nodeValue = "";
+  
   var firstName = document.getElementById("registerForm-FirstName").value;
   if (firstName === "") {
     errorMessage = "First Name is missing";
   }
   var LastName = document.getElementById("registerForm-LastName").value;
   if (LastName === "") {
-    errorMessage = "last Name is missing";
+    errorMessage = "Last Name is missing";
   }
   var Address = document.getElementById("registerForm-AddressLine1").value;
   if (Address === "") {
@@ -47,12 +47,13 @@ function validateRegistrationForm() {
   }
 
   var zip = document.getElementById("registerForm-Zip").value;
+
   if (zip === "") {
-    errorMessage = "state is missing";
-  }
-  var errorContainer = document.getElementById("fail");
-  
+    errorMessage = "zip is missing";
+  }  
+  var errorContainer = document.getElementById("fail");  
   errorContainer.style.display = "block";
   var text = document.createTextNode(errorMessage);
-  error.replaceChild(text);
+  error.textContent = errorMessage;
+  
 }
