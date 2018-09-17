6249054d8e4fff9f6e8c5dad557aa4d315d8af15
Andrew Ferguson <you@example.com>
2018-09-17 14:27:41 -0400

    validation fixes

--- web.js
+++ web.js
@@ -25,43 +25,67 @@ function validateRegistrationForm() {
   var error = document.getElementById("errorMessage");
   error.nodeValue = "";
   var firstName = document.getElementById("registerForm-FirstName").value;
+  var firstError = document.getElementById("registerForm-FirstName");
+
   if (firstName === "") {
-    var firstError = document.getElementById("registerForm-FirstName");
     firstError.style.border = "1px solid red";
     errorMessage = "First Name is missing.";
   }
+  else {
+    firstError.style.border = "";
+  }
+
   var LastName = document.getElementById("registerForm-LastName").value;
+  var lastError = document.getElementById("registerForm-LastName");
   if (LastName === "") {
     errorMessage = "Last Name is missing.";
-    var lastError = document.getElementById("registerForm-LastName");
     lastError.style.border = "1px solid red";
 
   }
+  else {
+    lastError.style.border = "";
+  }
+
   var Address = document.getElementById("registerForm-AddressLine1").value;
+  var addressError = document.getElementById("registerForm-AddressLine1");
+
   if (Address === "") {
-    var addressError = document.getElementById("registerForm-AddressLine1");
     addressError.style.border = "1px solid red";
     errorMessage = "Address Line 1 is missing.";
   }
+  else {
+    addressError.style.border = "";
+
+  }
   var city = document.getElementById("registerForm-City").value;
+  var cityError = document.getElementById("registerForm-City");
   if (city === "") {
-    var cityError = document.getElementById("registerForm-City");
     cityError.style.border = "1px solid red";
     errorMessage = "City is missing.";
   }
+  else {
+    cityError.style.border = "";
+  }
   var state = document.getElementById("registerForm-State").value;
+  var stateError = document.getElementById("registerForm-State");
   if (state === "") {
-    var stateError = document.getElementById("registerForm-State");
     stateError.style.border = "1px solid red";
     errorMessage = "State is missing.";
   }
+  else {
+    stateError.style.border = "";
+  }
 
   var zip = document.getElementById("registerForm-Zip").value;
+  var zipError = document.getElementById("registerForm-Zip");
+
   if (zip === "") {
     errorMessage = "Zip is missing.";
-    var zipError = document.getElementById("registerForm-Zip");
     zipError.style.border = "1px solid red";
-  }  
+  }
+  else {
+    zipError.style.border = "";
+  }
   var errorContainer = document.getElementById("fail");
   errorContainer.style.display = "none";
   if (errorMessage.length > 0) {
